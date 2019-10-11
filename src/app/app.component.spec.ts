596de98c443a6d587b722d85239e2a08ded0047d
emmanuelr.bts@gmail.com <emmanuel.lv.97@gmail.com>
2019-10-11 14:16:04 -0500

    scaffolding implemented

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'bts-admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bts-admin');
+    expect(app.title).toEqual('BTS Admin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bts-admin app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('BTS Admin');
   });
 });
